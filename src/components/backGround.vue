abca8e75491e3563167fc971f783db1411fd655c
lyle <2743292121@qq.com>
2023-12-08 16:20:48 +0800

    12_8

--- src/components/backGround.vue
+++ src/components/backGround.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="background">
     <!-- <van-button id="button1" class="">1</van-button> -->
-    <arrowButton1/>
+    <router-link to="/login">
+
+      <arrowButton1/>
+    </router-link>
+    <!-- <router-view></router-view> -->
+    
     <arrowButton2/>
     <arrowButton3/>
     <arrowButton4/>
@@ -9,27 +14,36 @@
     <arrowButton6/>
     <arrowButton7/>
     <arrowButton8/>
-    <img :src="require('../assets/background_pic.png')" class="pic">
+    <!-- <vButton1/> -->
+    <div class="cover">
+      <img :src="require('../assets/background_pic.png')" class="pic">
+    </div>
     <!-- <van-image class="pic" fit="contain" :src="require('../assets/background_pic.png')"/> -->
   </div>
-  
 </template>
+
 <script>
 </script>
 
 <style>
-@import url('../assets/arrowButton.css');
 .background {
   width: 1024px;
-  height: 768px;
+  height: 734px;
   margin: 0 auto;
   background-color: rgb(228, 226, 226);
 }
 .pic {
-  width: 650px;
-  position: absolute;
-  top: 120px;
+  /* width: 650px; */
+  width: 1024px;
+  margin: 0 auto;
+  opacity: 0.3;
+  filter: alpha(opacity=30);
+  /* position: absolute; */
+  /* top: 120px; */
   /* left: 50px; */
-  right: 955px;
+  /* right: 955px; */
+}
+.cover {
+  background: #c9c4c4;
 }
 </style>
